5001707488ac15caab189978f16b8c8cc60f85cb
Miroslav Dovger <miroslavdovger@gmail.com>
2023-10-22 22:51:36 +0200

    implement 1 and 2 with 1 failer test

--- src/main/java/uj/wmii/pwj/collections/JsonMapper.java
+++ src/main/java/uj/wmii/pwj/collections/JsonMapper.java
@@ -1,13 +1,65 @@
 package uj.wmii.pwj.collections;
 
-import java.util.Map;
+import java.util.*;
 
 public interface JsonMapper {
+    String openBrance = "{";
+    String closeBrance = "}";
 
     String toJson(Map<String, ?> map);
 
     static JsonMapper defaultInstance() {
-        return null;
-    }
+        return new JsonMapper() {
+            @Override
+            public String toJson(Map<String, ?> map) {
+                if(map == null) return "{}";
 
+                return toJsonValue(map);
+            }
+
+            private String toJsonValue(Object value){
+                if( value == null ){
+                    return "";
+                }else if(value instanceof String){
+                    return "\"" + value + "\"";
+                }else if(value instanceof Number || value instanceof Boolean){
+                    return value.toString();
+                }else if(value instanceof Map){
+                    Map<String, ?> map = (Map<String, ?>) value;
+
+                    StringBuilder json = new StringBuilder();
+                    json.append(openBrance);
+
+                    Iterator<? extends Map.Entry<String, ?>> itr = map.entrySet().iterator();
+
+                    while (itr.hasNext()) {
+                        Map.Entry<String, ?> entry = itr.next();
+                        json.append("\"").append(entry.getKey()).append("\": ").append(toJsonValue(entry.getValue()));
+                        if (itr.hasNext()) {
+                            json.append(",");
+                        }
+                    }
+
+                    json.append(closeBrance);
+                    return json.toString();
+                }else{
+                    StringBuilder sb = new StringBuilder();
+                    sb.append("[");
+
+                    List<Object> list = (List<Object>) value;
+                    Iterator<Object> itr = list.iterator();
+                    while(itr.hasNext()){
+                        sb.append(toJsonValue(itr.next()));
+                        if(itr.hasNext()){
+                            sb.append(", ");
+                        }
+                    }
+
+                    sb.append("]");
+                    return sb.toString();
+                }
+
+            }
+        };
+    }
 }
--- src/main/java/uj/wmii/pwj/collections/ListMerger.java
+++ src/main/java/uj/wmii/pwj/collections/ListMerger.java
@@ -1,10 +1,27 @@
 package uj.wmii.pwj.collections;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ListMerger {
     public static List<Object> mergeLists(List<?> l1, List<?> l2) {
-        return null;
-    }
+        if (l1 == null && l2 == null) return Collections.unmodifiableList(new ArrayList<>()); // empty list
+        else if (l1 == null) return List.copyOf(l2);
+        else if (l2 == null) return List.copyOf(l1);
 
+        ArrayList<Object> mergeResult = new ArrayList<>();
+
+        int size1 = l1.size();
+        int size2 = l2.size();
+        int maxSize = Math.max(size1, size2);
+
+        for (int i = 0; i < maxSize; i++) {
+            if (i < size1)
+                mergeResult.add(l1.get(i));
+            if (i < size2)
+                mergeResult.add(l2.get(i));
+        }
+        return Collections.unmodifiableList(mergeResult);
+    }
 }
